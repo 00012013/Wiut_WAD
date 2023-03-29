1477a25debc9801107f61c87934f6e05f4ef06e4
Sayidalokhon <sayidalokhonkham@gmail.com>
2023-03-29 17:44:41 +0500

    Created repository for department with all possible methods.

---
+++ CompanyManagementApp/Repository/DepartmentRepository.cs
@@ -0,0 +1,50 @@
+﻿using CompanyManagementApp.Data;
+using CompanyManagementApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyManagementApp.Repository
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+
+        private readonly MVCDbConetext mVCDbConetext;
+
+        public DepartmentRepository(MVCDbConetext mVCDbConetext)
+        {
+            this.mVCDbConetext = mVCDbConetext;
+        }
+
+        public void CreateDepartment(Department department)
+        {
+            mVCDbConetext.Departments.
+               Add(department);
+            mVCDbConetext.SaveChanges();
+        }
+
+        public void DeleteDepartment(Department department)
+        {
+            mVCDbConetext.Departments.Remove(department);
+            mVCDbConetext.SaveChanges();
+        }
+
+        public Task<Department> GetDepartment(Guid id)
+        {
+            return mVCDbConetext.Departments.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public Department GetDepartmentById(Guid id)
+        {
+            return mVCDbConetext.Departments.FirstOrDefault(x => x.Id == id);
+        }
+
+        public List<Department> GetDepartments()
+        {
+            return mVCDbConetext.Departments.ToList();
+        }
+
+        public Department GetDepartmentByName(string name)
+        {
+            return mVCDbConetext.Departments.FirstOrDefault(x => x.Name.Equals(name));
+        }
+    }
+}
