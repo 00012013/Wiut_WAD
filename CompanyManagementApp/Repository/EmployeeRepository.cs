fe3490bbfddaa329c857874a5dc4899f244f8ea9
Sayidalokhon <sayidalokhonkham@gmail.com>
2023-03-29 17:21:33 +0500

    Created Repository for Employee with all the possible methods.

---
+++ CompanyManagementApp/Repository/EmployeeRepository.cs
@@ -0,0 +1,45 @@
+﻿using CompanyManagementApp.Data;
+using CompanyManagementApp.Models;
+using CompanyManagementApp.Service;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyManagementApp.Repository
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly MVCDbConetext mVCDbConetext;
+
+        public EmployeeRepository(MVCDbConetext mVCDbConetext)
+        {
+            this.mVCDbConetext = mVCDbConetext;
+        }
+
+        public void CreateEmployee(Employee employee)
+        {
+            mVCDbConetext.Employees.Add(employee);
+            mVCDbConetext.SaveChanges();   
+        }
+
+        public void DeleteEmployee(Employee employee)
+        {
+            mVCDbConetext.Employees.Remove(employee);
+            mVCDbConetext.SaveChanges();
+          
+        }
+
+        public List<Employee> GetAll()
+        {
+            return mVCDbConetext.Employees.Include(e => e.Department).ToList();;
+        }
+
+        public Employee GetById(Guid id)
+        {
+           return mVCDbConetext.Employees.Include(e => e.Department).FirstOrDefault(x => x.Id == id);
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CompanyManagementApp/Repository/IEmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿using CompanyManagementApp.Models;
+
+namespace CompanyManagementApp.Repository
+{
+    public interface IEmployeeRepository
+    {
+        public List<Employee> GetAll();
+
+        public Employee GetById(Guid id);
+
+        public void CreateEmployee(Employee employee);
+
+        public void UpdateEmployee(Employee employee);
+    
+        public void DeleteEmployee(Employee employee);
+    }
+}
